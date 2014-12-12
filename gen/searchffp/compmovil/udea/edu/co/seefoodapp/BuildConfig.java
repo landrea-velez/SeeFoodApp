42d6b0a06bb043dc37a44aced098d58bb04da268
landrea-velez <landrea.velez@gmail.com>
2014-12-12 15:18:56 -0500

    Nueva configuración en Eclipse

---
+++ gen/searchffp/compmovil/udea/edu/co/seefoodapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package searchffp.compmovil.udea.edu.co.seefoodapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
